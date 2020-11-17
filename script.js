60d9e87a84ba6dbeeaa6e8beac0175b24e1c537e
AhmadK <ahtk81998@gmail.com>
2020-11-17 13:40:15 +0200

    Chall2

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function getPositives(arrayNumbers) {
           return item > 0;
         });
 }
+
+function handleMerge(e) {
+    let inputArray1 = document.getElementById("challenge21").value;
+    let inputArray2 = document.getElementById("challenge22").value;
+
+    let array1 = inputArray1.split(" ");
+    let array2 = inputArray2.split(" ");
+
+    let merged = array1.concat(array2);
+    document.getElementById("output2").innerHTML = merged;
+}
